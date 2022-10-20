d453dc477ffd1688af70cd12dd7fce81223a1068
KseniyaChepelevich <chepelevich.qamid.5@gmail.com>
2022-10-20 13:17:04 +0300

    Внесены изменения в метод IsAuthScreen

--- fmhandroid/app/src/androidTest/java/ru/iteco/fmhandroid/ui/steps/AuthSteps.java
+++ fmhandroid/app/src/androidTest/java/ru/iteco/fmhandroid/ui/steps/AuthSteps.java
@@ -1,23 +1,32 @@
 package ru.iteco.fmhandroid.ui.steps;
 
 import ru.iteco.fmhandroid.ui.data.DataHelper;
+import ru.iteco.fmhandroid.ui.data.ViewActions;
 import ru.iteco.fmhandroid.ui.page.AuthPageElements;
 
+import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
+import static androidx.test.espresso.matcher.ViewMatchers.withHint;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.not;
 
 public class AuthSteps {
     AuthPageElements authPageElements = new AuthPageElements();
 
     public void isAuthScreen() {
-        authPageElements.screenName.check(matches(isDisplayed()));
+        onView(isRoot()).perform(ViewActions.waitElement(withText("Authorization"), 10000));
+        onView(isRoot()).perform(ViewActions.waitElement(withHint("Login"), 10000));
+        onView(isRoot()).perform(ViewActions.waitElement(withHint("Password"), 10000));
+        onView(isRoot()).perform(ViewActions.waitElement(withText("SIGN IN"), 10000));
+        /*authPageElements.screenName.check(matches(isDisplayed()));
         authPageElements.loginField.check(matches(isDisplayed()));
         authPageElements.passField.check(matches(isDisplayed()));
-        authPageElements.signBtn.check(matches(isDisplayed()));
+        authPageElements.signBtn.check(matches(isDisplayed()));*/
     }
 
     public void authWithValidData (DataHelper.AuthInfo info) {
